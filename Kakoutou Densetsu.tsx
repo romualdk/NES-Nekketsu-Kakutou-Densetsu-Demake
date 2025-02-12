cba8154bf276e3f57960419b7f2b115e9f1b3688
Romuald Kowalczyk <romuald.kowalczyk@gmail.com>
2025-02-12 09:53:12 +0100

    Init

---
+++ Kakoutou Densetsu.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Kakoutou Densetsu" tilewidth="8" tileheight="8" tilecount="2048" columns="16">
+ <image source="Charecters.png" width="128" height="1024"/>
+</tileset>
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # NES-Nekketsu-Kakutou-Densetsu-Demake
+
+## Arenas
+
+### Arena 1
+
+![Arena 1](./Arenas/Arena 1.gif)
+
+496 x 176 px = 62 x 22 tiles
